44b2533fb6da72936f06faf02412b1f04deff960
Hermanto050302 <48467488+Hermanto050302@users.noreply.github.com>
2022-05-07 17:10:11 +0800

    update

---
+++ helpers/main.php
@@ -0,0 +1,27 @@
+<?php
+//function for get base path
+$basePath = 'http://localhost/tugas-perpustakaan/';
+
+function base_path()
+{
+    global $basePath;
+    return $basePath;
+}
+
+
+function asset()
+{
+    return base_path() . '/assets/';
+
+}
+
+//function for var_dump
+function dd($var)
+{
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+    die();
+}
+
+?>
